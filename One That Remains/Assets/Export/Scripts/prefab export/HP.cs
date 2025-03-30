719b4c6640206d6e8d2a10416d4a242b305e5032
L0rdSquidy <144682503+L0rdSquidy@users.noreply.github.com>
2025-03-30 18:11:39 +0200

    little changes

--- One That Remains/Assets/Export/Scripts/prefab export/HP.cs
+++ One That Remains/Assets/Export/Scripts/prefab export/HP.cs
@@ -6,85 +6,91 @@ using UnityEngine.SceneManagement;
 
 public class HP : MonoBehaviour
 {
-    // private TMP_Text scoreField;
-    private int hp = 3;
-    
-    private int Hitcounter;
+	// private TMP_Text scoreField;
+	private int hp = 3;
+	
+	private int Hitcounter;
 
-    [SerializeField] private GameObject player;
+	[SerializeField] private GameObject player;
 
-    [SerializeField] private Vector3 teleport_exit;
+	[SerializeField] private Vector3 teleport_exit;
 
-    [SerializeField] private GameObject UI;
+	[SerializeField] private GameObject UI;
 
-    [SerializeField] private GameObject Heart1;
+	[SerializeField] private GameObject Heart1;
 
-    [SerializeField] private GameObject Heart2;
+	[SerializeField] private GameObject Heart2;
 
-    [SerializeField] private GameObject Heart3;
+	[SerializeField] private GameObject Heart3;
 
-    [SerializeField] private GameObject HeartGameover;
+	[SerializeField] private GameObject HeartGameover;
 
-    [SerializeField] private PlayerMovementGrappling pmgMovement;
+	[SerializeField] private PlayerMovementGrappling pmgMovement;
 
-    [SerializeField] private PlayerCam pmgcam;
+	[SerializeField] private PlayerCam pmgcam;
 
-    public int getHp(){
-        return hp;
-    }
+	public int getHp(){
+		return hp;
+	}
 
-    public int returnHitcounter()
-    {
-        return Hitcounter;
-    }
-    public void AddScore(int add) {
-        
-        hp += add;
-        // scoreField.text = "" + hp;
-        Hitcounter -= add;
-        UI.SetActive(true);
-        StartCoroutine(HitCounterWait());
-    }
+	public int returnHitcounter()
+	{
+		return Hitcounter;
+	}
+	public void AddScore(int add) {
+		
+		hp += add;
+		// scoreField.text = "" + hp;
+		Hitcounter -= add;
+		UI.SetActive(true);
+		StartCoroutine(HitCounterWait());
+	}
 
-    IEnumerator HitCounterWait()
-    {
-        yield return new WaitForSeconds(1f);
-        player.SetActive(true);
-        Hitcounter = 0;
-        UI.SetActive(false);
-        player.transform.position = teleport_exit;
-    }
+	IEnumerator HitCounterWait()
+	{
+		yield return new WaitForSeconds(1f);
+		player.SetActive(true);
+		Hitcounter = 0;
+		UI.SetActive(false);
+		player.transform.position = teleport_exit;
+	}
 
-    public void setHp(int value)
-    {
-        hp = value;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if(hp >= 2)
-        {
-            Heart3.SetActive(false);
-        }else if(hp >= 1)
-        {
-            Heart3.SetActive(false);
-            Heart2.SetActive(false);
-        }else
-        {
-            Heart3.SetActive(false);
-            Heart2.SetActive(false);    
-            Heart1.SetActive(false);
-            HeartGameover.SetActive(true);
-            pmgMovement.enabled = false;
-            pmgcam.enabled = false;
-            StartCoroutine(bloh());
-        }
-    }
+	public void setHp(int value)
+	{
+		hp = value;
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		if (hp >= 3)
+		{
+			Heart3.SetActive(true);
+			Heart2.SetActive(true);    
+			Heart1.SetActive(true);
+		}
+		else if(hp >= 2)
+		{
+			Heart3.SetActive(false);
+		}else if(hp >= 1)
+		{
+			Heart3.SetActive(false);
+			Heart2.SetActive(false);
+		}else
+		{
+			Heart3.SetActive(false);
+			Heart2.SetActive(false);    
+			Heart1.SetActive(false);
+			HeartGameover.SetActive(true);
+			pmgMovement.enabled = false;
+			pmgcam.enabled = false;
+			StartCoroutine(bloh());
+		}
+	}
 
-    IEnumerator bloh()
-    {
-        yield return new WaitForSecondsRealtime(4f);
-        SceneManager.LoadScene("Main_Menu");
-        
-    }
+	IEnumerator bloh()
+	{
+		yield return new WaitForSecondsRealtime(4f);
+		SceneManager.LoadScene("Main_Menu");
+		
+	}
 }
